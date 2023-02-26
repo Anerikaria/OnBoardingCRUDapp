972ca0687d3d21ea214be51e883b5117acf36ab1
Anerikaria <karia.aneri@gmail.com>
2023-02-27 03:25:18 +1100

    Connection String added

--- Models/OnboardingDbContext.cs
+++ Models/OnboardingDbContext.cs
@@ -26,7 +26,7 @@ namespace OnBoardingDemo.Models
             if (!optionsBuilder.IsConfigured)
             {
 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=DESKTOP-EHJ5IHV\\ANERIPROJECTS;Database=OnbaordingDb;Trusted_Connection=True;MultipleActiveResultSets=true");
+                optionsBuilder.UseSqlServer("Server=tcp:onboardingcrudapp.database.windows.net,1433;Initial Catalog=OnBoardingCRUDapp;Persist Security Info=False;User ID=AneriK;Password=AnuK@240793;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             }
         }
 
